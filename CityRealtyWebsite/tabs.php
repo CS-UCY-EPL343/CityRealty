cb729355a234e1b54b6ba30c9688b436b352a953
pchris24 <pchris24@cs.ucy.ac.cy>
2017-02-10 23:46:19 +0200

    tabs php

---
+++ CityRealtyWebsite/tabs.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "USERNAME", "PASSWORD", "DATABASE");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "SELECT NOTES, TYPE, STATE, LOCATION, AAREA,FINALPRICE,BUILTDATE,DATEPUBLISHED FROM REALESTATE";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["NOTES"]."<br>";
+		echo "<br>";
+		echo "<br>";
+		echo "<br>";
+		echo $row["TYPE"]."<br>";
+		echo $row["STATE"]."<br>";
+		echo $row["LOCATION"]."<br>";
+		echo $row["AAREA"]."<br>";
+		echo $row["FINALPRICE"]."<br>";
+		echo $row["DATEBUILT"]."<br>";
+		echo $row["DATEPUBLISHED"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+ $conn->close();
+header("Location: propertyDetails.php");
+mysqli_close($conn);
+?>
+<?php
+?> 
