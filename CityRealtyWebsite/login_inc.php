3cbab645c2b5b3181d38db97b45ad7574c83e642
pchris24 <pchris24@cs.ucy.ac.cy>
2017-03-14 18:53:30 +0300

    login update

--- CityRealtyWebsite/login_inc.php
+++ CityRealtyWebsite/login_inc.php
@@ -10,13 +10,25 @@ if (!$conn) {
 $display_name = $_POST['display_name'];
 $password = $_POST['password'];
 
-$sql =  "SELECT * FROM User WHERE Username='$display_name' AND Password='$password'";
+$sql =  "SELECT * FROM UserSimple WHERE Username='$display_name' AND Password='$password'";
 $result = mysqli_query($conn, $sql); 
 
 if(!$row=mysqli_fetch_assoc($result)) {
 	$_SESSION['errorlogin']= "Your username or password is incorrect!";
 } else {
 	$_SESSION['id'] = $display_name;
+}
+
+if($_POST['username'] == $username && $_POST['password'] == $password){
+         header("Location: index.php");
+}else{
+	if($_SESSION['UserType'] == "ADMIN"){
+	   header("Location: admin.php");
+       header("Location: admin.html");
+    }else if($_SESSION['UserType'] == "External Broker"|| $_SESSION['UserType'] == "Internal Broker" || $_SESSION['UserType'] == "Broker"){
+			   header("Location: broker_manage.php");
+       header("Location: broker_manage.html");
+	}
 }
 
 header("Location: index.php");
