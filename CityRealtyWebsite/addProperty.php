e0b9d5a6cacc1c513be6416f75095306bcf694a9
pchris24 <pchris24@cs.ucy.ac.cy>
2017-03-28 15:44:32 +0300

    corrections in form fields on html and php

--- CityRealtyWebsite/addProperty.php
+++ CityRealtyWebsite/addProperty.php
@@ -1,36 +1,36 @@
 <?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
 session_start();
 
-$conn = mysqli_connect("localhost", "USERNAME", "PASSWORD", "DATABASE");
+$conn = mysqli_connect("localhost", "CityRealty", "QKSH7XJws7MCpxWR", "CityRealty");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 if(isset($_POST['send']))
 {
-    $realestate_no = $_POST['realestate_no'];
     $astreet_no = $_POST['astreet_no'];
     $astreet = $_POST['astreet'];
     $acity = $_POST['acity']; 
-    $area = $_POST['area'];
+    $area_tm = $_POST['area_tm'];
     $acountry = $_POST['acountry'];
     $apostal_code = $_POST['apostal_code'];
+	$aregion_code = $_POST['aregion_code'];
     $registration_title_no = $_POST['registration_title_no'];
     $piece_no = $_POST['piece_no'];
     $folder_no = $_POST['folder_no'];
-    $arithmos_fil_sxed = $_POST['arithmos_fil_sxed'];
+    $blueprint_no = $_POST['blueprint_no'];
     $arithmos_ad_oik = $_POST['arithmos_ad_oik'];
     $legal_control = $_POST['legal_control'];
     $initial_price = $_POST['initial_price'];
     $estimated_price = $_POST['estimated_price'];
-    $mortgage_loan = $_POST['mortgage_loan'];
+    $mortage_loan = $_POST['mortage_loan'];
     $objective_value = $_POST['objective_value'];
     $final_price = $_POST['final_price'];
-    $aview = $_POST['aview'];
     $corner = $_POST['corner'];
-    $subarea = $_POST['subarea'];
     $zone = $_POST['zone'];
-    $availability_from = $_POST['availability_from'];
+    $available_from = $_POST['available_from'];
     $status = $_POST['status'];
     $date_added = $_POST['date_added'];
     $registration_purpose = $_POST['registration_purpose'];
@@ -41,9 +41,8 @@ if(isset($_POST['send']))
     $vat = $_POST['vat'];
     $short_desc = $_POST['short_desc'];
     $desc = $_POST['desc'];
-    $for_rent = $_POST['for_rent'];
-    $view = $_POST['view'];
-    $contacted_via = $_POST['contacted_via'];
+    $about_renting = $_POST['about_renting'];
+    $recommended_by = $_POST['recommended_by'];
     $rating = $_POST['rating'];
     $registration_title = $_POST['registration_title'];
     $owner_id = $_POST['owner_id'];
@@ -51,44 +50,32 @@ if(isset($_POST['send']))
     $agent_rating = $_POST['agent_rating'];
     $order_code = $_POST['order_code'];
     $type_order = $_POST['type_order'];
-    $exclusive = $_POST['exclusive '];
-    $no_agent_reward = $_POST['no_agent_reward'];
+    $exclusive = $_POST['exclusive'];
     $order_start = $_POST['order_start'];
     $order_stop = $_POST['order_stop'];
-    $aarea = $_POST['aarea'];
     $category = $_POST['category'];
     $notes= $_POST['notes'];
-    $notify_colleauge= $_POST['notify_colleauge'];
-    $agent_id= $_POST['agent_id'];
-    $realestate_status = $_POST['realestate_status '];
+    $notify_colleague= $_POST['notify_colleague'];
+    $username= $_POST['username'];
+    $realestate_status = $_POST['realestate_status'];
     $agent_reward_percentage= $_POST['agent_reward_percentage'];
     $agent_reward= $_POST['agent_reward'];
     $view_address= $_POST['view_address'];
-    $estate_no= $_POST['estate_no'];
     $transfer_cost= $_POST['transfer_cost'];
     $transfer_vat= $_POST['transfer_vat'];
     $consideration_exchange= $_POST['consideration_exchange'];
-}
-
-$sql = "INSERT INTO REALESTATE (RealEstateNo, AStreetNo, AStreet, Acity, Area, Country, APostalCode, RegistrationTitleNo, PieceNo, FolderNo, ArithmosFilSxed, ArithmosAdOik, LegalControl,
-InitialPrice, EstimatedPrice, MortgageLoan, ObjectiveValue, FinalPrice, AView, Corner, SubArea, AvailabilityFrom, Status, DateAdded, RegistrationPurpose, Price, Currency, PricePerTM, TM, VAT,
-ShortDescription, Description, ForRent, View, ContactedVia, Rating, RegistartionTitle, OwnerID, OwnerRating, AgentRating, OrderCode, TypeCode, Exclusive, NoAgentReward, OrderStart,
-OrderStop, AArea, Category, Notes, NotifyColleague, AgentID, RealEstateStatus, AgentRewardPercentage, AgentReward, ViewAddress, EstateNo,TransferCost, TransferVAT, ConsiderationExchange)
-VALUES ('$realestate_no', '$astreet_no', '$astreet', '$acity', '$area', '$acountry', '$apostal_code', '$registration_title_no', '$piece_no', '$folder_no', '$arithmos_fil_sxed', '$arithmos_ad_oik', '$legal_control', '$initial_price', '$estimated_price', '$mortgage_loan', '$objective_value', '$final_price', '$aview', '$corner', '$subarea, $zone', '$availability_from', '$status','$date_added', 
-'$registration_purpose', '$price', '$currency', '$price_per_tm', '$tm', '$vat', '$short_desc', '$desc', '$for_rent', '$view', '$contacted_via', '$rating', '$registration_title', '$owner_id', 
-'$owner_rating', '$agent_rating', '$order_code', '$type_order', '$exclusive', '$no_agent_reward', '$order_start', '$order_stop', '$aarea', '$category', '$notes', '$notify_colleauge', '$agent_id', '$realestate_status', '$agent_reward_percentage', '$agent_reward', '$view_address', '$estate_no', '$transfer_cost', '$transfer_vat', '$consideration_exchange')";
 
 
-mysql_select_db('CityRealty');
-$retval = mysql_query( $sql, $conn );
-if(! $retval )
-{
-  die('Could not enter data: ' . mysql_error());
-}else{
-echo "Entered data successfully\n";
-}
+$sql = "INSERT INTO RealEstate(AStreetNo, AStreet, Acity, AreaTM, Country, APostalCode, ARegionCode,RegistrationTitleNo, PieceNo, FolderNo, ArithmosFilSxed, ArithmosAdOik, LegalControl,
+InitialPrice, EstimatedPrice, MortgageLoan, ObjectiveValue, FinalPrice,Corner, AvailableFrom, Status, DateRegistered, RegistrationPurpose, Price, Currency, PricePerTM, TM, VAT,
+ShortDescription, Description, AboutRenting, RecommendedBy, Rating, RegistartionTitle, OwnerID, OwnerRating, AgentRating, OrderCode, TypeCode, Exclusive, OrderStart,
+OrderStop, Category, Notes, NotifyColleague, AgentUsername, RealEstateStatus, AgentRewardPercentage, AgentReward, ViewAddress,TransferCost, TransferVAT, ConsiderationExchange)
+VALUES ('$astreet_no', '$astreet', '$acity', '$area_tm', '$acountry', '$apostal_code', '$aregion_code','$registration_title_no', '$piece_no', '$folder_no', '$blueprint_no', '$arithmos_ad_oik', '$legal_control', '$initial_price', '$estimated_price', '$mortage_loan', '$objective_value', '$final_price','$corner', $zone', '$available_from', '$status','$date_added', 
+'$registration_purpose', '$price', '$currency', '$price_per_tm', '$tm', '$vat', '$short_desc', '$desc', '$about_renting', '$recommended_by', '$rating', '$registration_title', '$owner_id', 
+'$owner_rating', '$agent_rating', '$order_code', '$type_order', '$exclusive','$order_start', '$order_stop','$category', '$notes', '$notify_colleague', '$username', '$realestate_status', '$agent_reward_percentage', '$agent_reward', '$view_address','$transfer_cost', '$transfer_vat', '$consideration_exchange')";
 
-header("Location: addProperty.php");
+$result = mysqli_query($conn,$sql)
+  or die("Error: ".mysqli_error($conn));
 
 mysqli_close($conn);
 }
