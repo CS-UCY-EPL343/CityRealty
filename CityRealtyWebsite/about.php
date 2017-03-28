943cc8c89e869fde18513b0be41fc0d99c8884b4
sgrego02 <sgrego02@cs.ucy.ac.cy>
2017-03-29 02:27:47 +0300

    Update about.php

--- CityRealtyWebsite/about.php
+++ CityRealtyWebsite/about.php
@@ -109,27 +109,27 @@ session_start();
 				</li>
 
 				<li>
-					<a class="page-scroll" href="#services" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/sale22.png">Sale</span></a>
+					<a class="page-scroll" href="sale.php" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/sale22.png">Sale</span></a>
 
 				</li>
 				<li>
-					<a class="page-scroll " href="#services" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/rent22.png">Rent </span></a>
+					<a class="page-scroll " href="rent.php" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/rent22.png">Rent </span></a>
 
 				</li>
 				<li>
-					<a class="page-scroll " href="AddProperty.html" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/sell22.png">Sell</span></a>
+					<a class="page-scroll " href="sell.php" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/sell22.png">Sell</span></a>
 
 				</li>
 				<li>
-					<a class="page-scroll " href="index.php#searchREForm" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/search2.png">Search</a>
+					<a class="page-scroll " href="index.php#search" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/search2.png">Search</a>
 				</li>
 
 				<li>
-					<a class="page-scroll" href="index.php#contactForm" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/contactUs2.png">Contact Us</a>
+					<a class="page-scroll" href="index.php#contact" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/contactUs2.png">Contact Us</a>
 				</li>
 
 				<li>
-					<a class="page-scroll" href="about.php" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/group-chat.png"> About Us</a>
+					<a class="page-scroll" href="#" style="color: #FFCC00"><img width="50px" height="50px" src="img/menu/group-chat.png"> About Us</a>
 				</li>
 
 				<?php
@@ -153,16 +153,8 @@ session_start();
 				<?php
 				if(!isset($_SESSION['id'])) {
 					echo"
-					<li class='dropdown'>
-						<a href='javascript:void(0)' class='dropbtn' onclick='DropDownFunction()' style='color: #FFCC00'><span class='glyphicon glyphicon-user' aria-hidden='true'></span> SignUp</a>
-						<div class='dropdown-content' id='myDropdown'>
-							<a href='#SignUp-modal' data-toggle='modal' data-target='#my-modal-lg' style='color: #FFCC00'>As A Client</a>
-							<a class='page-scroll' href='#ExBroker-modal' data-toggle='modal' data-target='#ExBromodal-lg' style='color: #FFCC00'>As An Agent </span></a>
-						</div>
-					</li>
-					<li>
-
-						<a href='#LogIn-modal' data-toggle='modal' data-target='#my-logInmodal-sm' style='color: #FFCC00'>Log in</a>";
+					<li><a href='#SignUp-modal' data-toggle='modal' data-target='#my-modal-lg' style='color: #FFCC00'>SignUp</a></li>
+					<li><a href='#LogIn-modal' data-toggle='modal' data-target='#my-logInmodal-sm' style='color: #FFCC00'>Log in</a></li>";
 					}
 					else {
 						echo "<li><a href='#' style='color: #FFCC00'>" . $_SESSION['id'] . "</a></li><li>
@@ -410,233 +402,6 @@ session_start();
 		</div>
 	</div>
 	<!----------------------------end of LogIn---------------------------------->
-
-	<!----------------------------Sign Up Broker---------------------------------->
-	<div class="modal my-ExBromodal-lg" id="ExBromodal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg">
-			<div class="modal-content">
-
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">
-						×
-					</button>
-					<h3>Sign Up As A Broker</h3>
-				</div>
-
-				<div class="modal-body">
-					<form role="form" action="signup2_inc.php" method="POST">
-						<div class="row">
-							<small>&nbsp&nbsp&nbsp&nbsp&nbspYOUR NAME</small>
-							<br>
-							<div class="col-xs-12 col-sm-6 col-md-6">
-								<div class="form-group">
-									<input type="text" name="first_name" id="first_name" class="form-control input-lg" placeholder="First Name" tabindex="1" autocomplete="on">
-									<input type="text" name="last_name" id="last_name" class="form-control input-lg" placeholder="Last Name" tabindex="2" autocomplete="on">
-								</div>
-							</div>
-						</div>
-						<p></p>
-						<small>CHOOSE A USERNAME</small>
-						<span runat="server" ID="required" style="color:Red;" visible="false"> *</span>
-						<div class="form-group">
-							<input type="text" name="display_name" id="display_name" class="form-control input-lg" placeholder="Username" tabindex="3" required>
-						</div>
-						<small>GENDER</small>
-						<span runat="server" ID="required" style="color:Red;" visible="false"> *</span>
-						<p></p>
-						<div class="col-md-8 ">
-							<label>
-								<input type="radio" name="gender" value="0" required>
-								Male
-							</label>
-							<label>
-								<input type="radio" name="gender" value="1" required>
-								Female 
-							</label>
-						</div>
-						<p>
-							<br>
-						</p>
-						<small>DATE OF BIRTH	:</small>
-						<select class="form-control" name="month">
-							<option value="01">January</option>
-							<option value="02">February</option>
-							<option value="03">March</option>
-							<option value="04">April</option>
-							<option value="05">May</option>
-							<option value="06">June</option>
-							<option value="07">July</option>
-							<option value="08">August</option>
-							<option value="09">September</option>
-							<option value="10">October</option>
-							<option value="11">November</option>
-							<option value="12">December</option>
-							<option selected value="">---</option>
-						</select>
-						<select name="day" class="form-control">
-							<option value="01">01</option>
-							<option value="02">02</option>
-							<option value="03">03</option>
-							<option value="04">04</option>
-							<option value="05">05</option>
-							<option value="06">06</option>
-							<option value="07">07</option>
-							<option value="08">08</option>
-							<option value="09">09</option>
-							<option value="10">10</option>
-							<option value="11">11</option>
-							<option value="12">12</option>
-							<option value="13">13</option>
-							<option value="14">14</option>
-							<option value="15">15</option>
-							<option value="16">16</option>
-							<option value="17">17</option>
-							<option value="18">18</option>
-							<option value="19">19</option>
-							<option value="20">20</option>
-							<option value="21">21</option>
-							<option value="22">22</option>
-							<option value="23">23</option>
-							<option value="24">24</option>
-							<option value="25">25</option>
-							<option value="26">26</option>
-							<option value="27">27</option>
-							<option value="28">28</option>
-							<option value="29">29</option>
-							<option value="30">30</option>
-							<option value="31">31</option>
-							<option selected value="">--</option>
-						</select>
-						<select name="year" class="form-control">
-							<option value="2012">2016</option>
-							<option value="2012">2015</option>
-							<option value="2012">2014</option>
-							<option value="2012">2013</option>
-							<option value="2012">2012</option>
-							<option value="2011">2011</option>
-							<option value="2010">2010</option>
-							<option value="2009">2009</option>
-							<option value="2008">2008</option>
-							<option value="2007">2007</option>
-							<option value="2006">2006</option>
-							<option value="2005">2005</option>
-							<option value="2004">2004</option>
-							<option value="2003">2003</option>
-							<option value="2002">2002</option>
-							<option value="2001">2001</option>
-							<option value="2000">2000</option>
-							<option value="1999">1999</option>
-							<option value="1998">1998</option>
-							<option value="1997">1997</option>
-							<option value="1996">1996</option>
-							<option value="1995">1995</option>
-							<option value="1994">1994</option>
-							<option value="1993">1993</option>
-							<option value="1992">1992</option>
-							<option value="1991">1991</option>
-							<option value="1990">1990</option>
-							<option value="1989">1989</option>
-							<option value="1988">1988</option>
-							<option value="1987">1987</option>
-							<option value="1986">1986</option>
-							<option value="1985">1985</option>
-							<option value="1984">1984</option>
-							<option value="1983">1983</option>
-							<option value="1982">1982</option>
-							<option value="1981">1981</option>
-							<option value="1980">1980</option>
-							<option value="1979">1979</option>
-							<option value="1978">1978</option>
-							<option value="1977">1977</option>
-							<option value="1976">1976</option>
-							<option value="1975">1975</option>
-							<option value="1974">1974</option>
-							<option value="1973">1973</option>
-							<option value="1972">1972</option>
-							<option value="1971">1971</option>
-							<option value="1970">1970</option>
-							<option value="1969">1969</option>
-							<option value="1968">1968</option>
-							<option value="1967">1967</option>
-							<option value="1966">1966</option>
-							<option value="1965">1965</option>
-							<option value="1964">1964</option>
-							<option value="1963">1963</option>
-							<option value="1962">1962</option>
-							<option value="1961">1961</option>
-							<option value="1960">1960</option>
-							<option value="1959">1959</option>
-							<option value="1958">1958</option>
-							<option value="1957">1957</option>
-							<option value="1956">1956</option>
-							<option value="1955">1955</option>
-							<option value="1954">1954</option>
-							<option value="1953">1953</option>
-							<option value="1952">1952</option>
-							<option value="1951">1951</option>
-							<option value="1950">1950</option>
-							<option selected value="">---</option>
-						</select>
-
-						<p></p>
-						<small>EMAIL ADDRESS</small>
-						<span runat="server" ID="required" style="color:Red;" visible="false"> *</span>
-						<br>
-						<div class="form-group">
-							<input type="email" name="email" id="email" class="form-control input-lg" placeholder="e.g. example@domain.com" tabindex="4" autocomplete="on" size="47" required>
-						</div>
-						<p></p>
-						<small>PHONE</small>
-						<span runat="server" ID="required" style="color:Red;" visible="false"> *</span>
-						<br>
-						<div class="form-group">
-							<input type="tel" name="phone" id="phone" class="form-control input-lg" placeholder="Phone Number" tabindex="4" required>
-						</div>
-						<div class="row">
-							<p></p>
-							<small>&nbsp&nbsp&nbsp&nbsp&nbspCREATE A PASSWORD</small>
-							<span runat="server" ID="required" style="color:Red;" visible="false"> *</span>
-							<br>
-							<div class="col-xs-12 col-sm-6 col-md-6">
-								<div class="form-group">
-									<input type="password" name="password2" id="password2" class="form-control input-lg" placeholder="Password" tabindex="5" required pattern=".{6,}">
-								</div>
-							</div>
-							<div class="col-xs-12 col-sm-6 col-md-6">
-								<div class="form-group">
-									<input type="password" name="password_confirmation2" id="password_confirmation2" class="form-control input-lg" placeholder="Confirm Password" tabindex="6" required>
-								</div>
-							</div>
-
-						</div>
-						<div class="row">
-							<div class="col-xs-8 col-sm-9 col-md-9">
-								<p>
-									<span class="button-checkbox">
-										<input type="checkbox" name="t_and_c" id="t_and_c"  value="1" required>
-									</span>
-									<i> I agree to the <a href="http://www.omegarealestate.gr/en/" data-toggle="modal" data-target="#t_and_c_m"> Terms and Conditions</a> set out by this site, including the Cookie Use. 
-									</i>
-								</p>
-							</div>
-						</div>
-
-						<div class="modal-footer">
-							<div class="col-xs-12 col-md-6">
-								<input type="submit" value="Sign Up" class="btn btn-primary btn-block btn-lg" tabindex="7">
-							</div>
-							<div class="col-xs-12 col-md-6">
-								<input type="reset" value="Reset" class="btn btn-primary btn-block btn-lg" tabindex="7">
-
-								<!--<input type="reset" value="Reset" class="btn btn-default btn-lg" tabindex="7">-->
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!----------------------------end of Sign Up Broker---------------------------------->
 
 	<!-- jQuery -->
 	<script src="js/jquery.js"></script>
