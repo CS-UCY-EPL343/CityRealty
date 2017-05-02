0fcbf84e5265f9e5c13f294513938adc2700ec52
sgrego02 <sgrego02@cs.ucy.ac.cy>
2017-05-02 20:17:37 +0300

    Update signup_inc.php

--- CityRealtyWebsite/signup_inc.php
+++ CityRealtyWebsite/signup_inc.php
@@ -15,20 +15,33 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $rdate = date("Y-m-d", time());
 
-$sql =  "SELECT * FROM UserSimple WHERE Username ='$display_name' OR Email='$email'";
-$result = mysqli_query($conn, $sql); 
+if ($sql = $conn->prepare("SELECT UserType FROM UserSimple WHERE Username =? OR Email=?")) {
+	$sql->bind_param("ss", $display_name, $email);
+	$sql->execute();
+	$sql->bind_result($row);
+	$sql->fetch();
 
-if(!$row=mysqli_fetch_assoc($result)) {
-	$sql2 =  "INSERT INTO UserSimple (Username, Password, Name, Surname, Email, UserType, UserStatus, RegistrationDate, LastLoggedin)
-	VALUES ('$display_name', '$password', '$first_name', '$last_name', '$email', 'Simple User', 'Active User', '$rdate', '$rdate')";
-	$result2 = mysqli_query($conn, $sql2); 
-	$_SESSION['signedup'] = "You've successfully signed up!";
-	mysqli_close($conn);
-	header("Location: index.php");
-} else {
-	$_SESSION['errormessage'] = "Username or email already used!";
-	mysqli_close($conn);
-	header("Location: index.php");
-	
+	if(!$row) {
+		if ($sql2 = $conn->prepare("INSERT INTO UserSimple (Username, Password, Name, Surname, Email) VALUES (?, ?, ?, ?, ?)")) {
+			$sql2->bind_param("sssss", $display_name, $password, $first_name, $last_name, $email);
+			$sql2->execute();
+			$sql3 = $conn->prepare("UPDATE UserSimple SET UserType='Simple User', UserStatus='Active User', RegistrationDate='$rdate', LastLoggedin='$rdate' WHERE Username =?");
+			$sql3->bind_param("s", $display_name);
+			$sql3->execute();
+			$_SESSION['signedup'] = "You've successfully signed up!";
+			mysqli_close($conn);
+			$sql->close();
+			$sql2->close();
+			$sql3->close();
+			header("Location: index.php");
+		}
+	} else {
+		$_SESSION['errormessage'] = "Username or email already used!";
+		mysqli_close($conn);
+		$sql->close();
+		header("Location: index.php");
+
+	}
+
 }
 ?>
