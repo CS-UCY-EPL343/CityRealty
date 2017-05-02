21027ecaebf4e0d4ff9a39ec0fde74a752f90b78
sgrego02 <sgrego02@cs.ucy.ac.cy>
2017-05-02 20:18:56 +0300

    Update signup2_inc.php

--- CityRealtyWebsite/signup2_inc.php
+++ CityRealtyWebsite/signup2_inc.php
@@ -4,7 +4,7 @@ session_start();
 $conn = mysqli_connect("localhost", "USERNAME", "PASSWORD", "DATABASE");
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+	die("Connection failed: " . mysqli_connect_error());
 }
 
 mysqli_query($conn,"SET NAMES utf8");
@@ -26,22 +26,35 @@ $City = $_POST['City'];
 $country_name = $_POST['country_name'];
 $phone = $_POST['phone'];
 
-$sql =  "SELECT * FROM UserSimple WHERE Username ='$display_name' OR Email='$email'";
-$result = mysqli_query($conn, $sql); 
+if ($sql = $conn->prepare("SELECT UserType FROM UserSimple WHERE Username =? OR Email=?")) {
+	$sql->bind_param("ss", $display_name, $email);
+	$sql->execute();
+	$sql->bind_result($row);
+	$sql->fetch();
 
-if(!$row=mysqli_fetch_assoc($result)) {
-	$sql2 =  "INSERT INTO UserSimple (Username, Password, Name, Surname, Email, UserType, UserStatus, RegistrationDate, LastLoggedin)
-	VALUES ('$display_name', '$password', '$first_name', '$last_name', '$email', 'External Agent', 'Active User', '$rdate', '$rdate')";
-	$result2 = mysqli_query($conn, $sql2);
-	$sql3 = "INSERT INTO UserExtended (Username, PrivatePhone, ANumber, AStreet, ACity, ACountry) VALUES ('$display_name', '$phone', '$street_num', '$street', '$City', '$country_name')";
-	$result3 = mysqli_query($conn, $sql3); 
-	$_SESSION['signedup'] = "You have successfully signed up!";
-	header("Location: index.php");
-} else {
-	$_SESSION['errormessage'] = "Username or email already used!";
-	header("Location: index.php");
-	
-}
-
-mysqli_close($conn);
+	if(!$row) {
+		if ($sql2 = $conn->prepare("INSERT INTO UserSimple (Username, Password, Name, Surname, Email) VALUES (?, ?, ?, ?, ?)")) {
+			$sql2->bind_param("sssss", $display_name, $password, $first_name, $last_name, $email);
+			$sql2->execute();
+			$sql3 = $conn->prepare("UPDATE UserSimple SET UserType='Simple User', UserStatus='Active User', RegistrationDate='$rdate', LastLoggedin='$rdate' WHERE Username =?");
+			$sql3->bind_param("s", $display_name);
+			$sql3->execute();
+			$sql4 = $conn->prepare("INSERT INTO UserExtended (Username, PrivatePhone, ANumber, AStreet, ACity, ACountry) VALUES (?, ?, ?, ?, ?, ?)");
+			$sql4->bind_param("sdssss", $display_name, $phone, $street_num, $street, $City, $country_name);
+			$sql4->execute();
+			$_SESSION['signedup'] = "You've successfully signed up!";
+			mysqli_close($conn);
+			$sql->close();
+			$sql2->close();
+			$sql3->close();
+			$sql4->close();
+			header("Location: index.php");
+		}
+	} else {
+		$_SESSION['errormessage'] = "Username or email already used!";
+		mysqli_close($conn);
+		$sql->close();
+		header("Location: sell.php");
+	}
+} 
 ?>
