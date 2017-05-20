a0553f56c51e63cbde07210188a27a38da5abaff
pchris24 <pchris24@cs.ucy.ac.cy>
2017-05-20 15:50:28 +0300

    Update mail_handler.php

--- CityRealtyWebsite/mail_handler.php
+++ CityRealtyWebsite/mail_handler.php
@@ -2,14 +2,14 @@
 if(isset($_POST['submit'])){
     $to = "pchris24@cs.ucy.ac.cy"; // this is your Email address
     $from = $_POST['mail']; // this is the sender's Email address
-    $name = $_POST['name'];
+    $onoma = $_POST['onoma'];
     $phone = $_POST['phone'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = "". $_POST['name'] . " wrote the following:" . "\n\n" . $_POST['message'] . "\n\n" . $_POST['phone'];
+    $message = "". $_POST['onoma'] . " wrote the following:" . "\n\n" . $_POST['message'] . "\n\n" . $_POST['phone'];
  
     $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers,$name);
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    mail($to,$subject,$message,$headers,$onoma);
+    echo "Mail Sent. Thank you " . $onoma . ", we will contact you shortly.";
     }
 ?>
