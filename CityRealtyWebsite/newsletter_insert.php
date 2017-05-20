0bf693a8f028e846f04d3abcd4c00cc27c0319a6
Erasmia Shimitra <erasmia_10@hotmail.com>
2017-05-20 15:53:44 +0300

    Newsletter Insert function

---
+++ CityRealtyWebsite/newsletter_insert.php
@@ -0,0 +1,24 @@
+<?php 
+	ini_set('error_reporting', E_ALL);
+	ini_set('display_errors', 1);
+	session_start();
+	
+	$conn = mysqli_connect("localhost", "CityRealty", "QKSH7XJws7MCpxWR", "CityRealty");
+	mysqli_query($conn,"SET NAMES utf8");
+	
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	if(isset($_POST['newsletter_button']))
+	{
+		$email = $_POST['nemail'];
+		$name = $_POST['nname'];
+	
+		$sql = "INSERT INTO NewsletterList (Email,Name) VALUES ('$email','$name');";
+
+		$result = mysqli_query($conn,$sql)
+		or die("Error: !".mysqli_error($conn));
+		$_SESSION['addedNewsletter'] = "You've successfully registered to our Newsletter!";
+		mysqli_close($conn);
+	}
+?>
