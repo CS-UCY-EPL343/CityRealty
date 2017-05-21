048417251c47d3cf47100ec95c33788ebe9fd1b6
sgrego02 <sgrego02@cs.ucy.ac.cy>
2017-05-21 17:12:17 +0300

    Update AddPropertyFormTab1.php

--- CityRealtyWebsite/AddPropertyFormTab1.php
+++ CityRealtyWebsite/AddPropertyFormTab1.php
@@ -1,260 +1,347 @@
-<?php session_start(); ?>
+<?php session_start(); 
+$conn = mysqli_connect("localhost", "USERNAME", "PASSWORD", "DATABASE");
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+mysqli_query($conn,"SET NAMES utf8");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
-	<head>
+<head>
 
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="">
-		<meta name="author" content="">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
 
-		<title>Insert Property</title>
+	<title>Insert Property</title>
 
-		<!-- Bootstrap Core CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
 
-		<!-- Custom CSS -->
-		<link href="css/sb-admin.css" rel="stylesheet">
+	<!-- Custom CSS -->
+	<link href="css/sb-admin.css" rel="stylesheet">
 
-		<!-- Custom Fonts -->
-		<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<!-- Custom Fonts -->
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-		<!--Navbar style-->
-		<style type="text/css">
-			#menu {
-				overflow: hidden;
-			}
-			#menu li {
-				display: block;
-				position: relative;
-				float: left;
-			}
-			#menu li:first-child {
-				margin-left: 0px;
-				border-bottom: solid #000000
-			}
-			#menu li:last-child {
-				margin-left: 100px;
-			}
-			#menu li a {
-				display: block;
-				color: #333;
-				font-size: 18px;
-				text-align: center;
-				text-decoration: none;
-				text-transform: uppercase;
-			}
-			#menu li span {
-				display: none;
-			}
-			#menu li.active span {
-				display: block;
-				position: absolute;
-				width: 100%;
-				text-align: center;
-			}
-			#menu li.active a {
-				background-color: #c03e62;
-				outline: #FFF;
-				outline-style: dashed;
-				color: #CCC;
-				text-decoration: none;
-			}
-			#menu li a:hover {
-				text-decoration: none;
-				background-image: none;
-				background-color: #c03e62;
-				outline: #FFF;
-			}
-		</style>
+	<!--Navbar style-->
+	<style type="text/css">
+		#menu {
+			overflow: hidden;
+		}
+		#menu li {
+			display: block;
+			position: relative;
+			float: left;
+		}
+		#menu li:first-child {
+			margin-left: 0px;
+			border-bottom: solid #000000
+		}
+		#menu li:last-child {
+			margin-left: 100px;
+		}
+		#menu li a {
+			display: block;
+			color: #333;
+			font-size: 18px;
+			text-align: center;
+			text-decoration: none;
+			text-transform: uppercase;
+		}
+		#menu li span {
+			display: none;
+		}
+		#menu li.active span {
+			display: block;
+			position: absolute;
+			width: 100%;
+			text-align: center;
+		}
+		#menu li.active a {
+			background-color: #c03e62;
+			outline: #FFF;
+			outline-style: dashed;
+			color: #CCC;
+			text-decoration: none;
+		}
+		#menu li a:hover {
+			text-decoration: none;
+			background-image: none;
+			background-color: #c03e62;
+			outline: #FFF;
+		}
+	</style>
 
-		<!--Dropdown style-->
-		<style>
-			.dropdown-content {
-				list-style: none;
-				display: none;
-				position: absolute;
-				background-color: #f9f9f9;
-				min-width: 160px;
-				box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-			}
-			.dropdown-content a {
-				color: #337ab7;
-				padding: 12px 16px;
-				text-decoration: none;
-				display: block;
-				text-align: left;
-			}
-			.dropdown-content a:hover {
-				background-color: #f1f1f1
-			}
-			.dropdown:hover .dropdown-content {
-				display: block;
-			}
+	<!--Dropdown style-->
+	<style>
+		.dropdown-content {
+			list-style: none;
+			display: none;
+			position: absolute;
+			background-color: #f9f9f9;
+			min-width: 160px;
+			box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+		}
+		.dropdown-content a {
+			color: #337ab7;
+			padding: 12px 16px;
+			text-decoration: none;
+			display: block;
+			text-align: left;
+		}
+		.dropdown-content a:hover {
+			background-color: #f1f1f1
+		}
+		.dropdown:hover .dropdown-content {
+			display: block;
+		}
 
-			#content #mainwrap #tabs .column.col3 #title2 #title3 #title4 h2 {
-				font-family: Arial, Helvetica, sans-serif;
-				font-size: 26em;
-				font-style: normal;
-				line-height: normal;
-				font-weight: bold;
-				font-variant: normal;
-			}
-			#content #mainwrap #RealEstateTab .column.col3 #title2 h2 {
-				font-size: large;
-				font-style: normal;
-				line-height: normal;
-				font-weight: bold;
-				font-variant: normal;
-				text-decoration: underline;
-			}
-			#content #mainwrap #RealEstateTab {
-				border: 5px solid black;
-				border-style: solid;
-				background: #FFFFFF;
-			}
-			#content #mainwrap #tabs {
-				border: 5px solid black;
-				border-style: solid;
-				background: #FFFFFF;
-			}
-			.fieldGroup {
-				color: #c03e62;
-			}
-			.field {
-				color: #000000;
-				width: 30%;
-			}
-			.value {
-				color: #333333;
-				text-decoration: none;
-			}
-			table {
-				border: 0px solid black;
-				padding: 5px;
-				text-align: left;
-				width: 100%;
-			}
-			th, td {
-				border: 0px solid black;
-				padding: 3px;
-				text-align: left;
-			}
+		#content #mainwrap #tabs .column.col3 #title2 #title3 #title4 h2 {
+			font-family: Arial, Helvetica, sans-serif;
+			font-size: 26em;
+			font-style: normal;
+			line-height: normal;
+			font-weight: bold;
+			font-variant: normal;
+		}
+		#content #mainwrap #RealEstateTab .column.col3 #title2 h2 {
+			font-size: large;
+			font-style: normal;
+			line-height: normal;
+			font-weight: bold;
+			font-variant: normal;
+			text-decoration: underline;
+		}
+		#content #mainwrap #RealEstateTab {
+			border: 5px solid black;
+			border-style: solid;
+			background: #FFFFFF;
+		}
+		#content #mainwrap #tabs {
+			border: 5px solid black;
+			border-style: solid;
+			background: #FFFFFF;
+		}
+		.fieldGroup {
+			color: #c03e62;
+		}
+		.field {
+			color: #000000;
+			width: 30%;
+		}
+		.value {
+			color: #333333;
+			text-decoration: none;
+		}
+		table {
+			border: 0px solid black;
+			padding: 5px;
+			text-align: left;
+			width: 100%;
+		}
+		th, td {
+			border: 0px solid black;
+			padding: 3px;
+			text-align: left;
+		}
 
-			#content #mainwrap #menu {
-				padding: 20px;
-				overflow: hidden;
-			}
-			#mainwrap {
-				overflow: hidden;
-				position: relative;
-				margin: 0 auto;
-			}
-			#content {
-				overflow: hidden;
-				position: relative;
-			}
+		#content #mainwrap #menu {
+			padding: 20px;
+			overflow: hidden;
+		}
+		#mainwrap {
+			overflow: hidden;
+			position: relative;
+			margin: 0 auto;
+		}
+		#content {
+			overflow: hidden;
+			position: relative;
+		}
 
-			#pagecontainer {
-				position: relative;
-				width: 9999px;
-			}
+		#pagecontainer {
+			position: relative;
+			width: 9999px;
+		}
 
-			.section {
-				float: left;
-				position: relative;
-				width: 100%;
-				padding: 30px;
-				overflow: hidden;
-			}
+		.section {
+			float: left;
+			position: relative;
+			width: 100%;
+			padding: 30px;
+			overflow: hidden;
+		}
 
-			#RealEstateTab #tabs {
-				overflow-y: scroll;
-			}
+		#RealEstateTab #tabs {
+			overflow-y: scroll;
+		}
 
-			#tabs {
-				display: none;
-			}
+		#tabs {
+			display: none;
+		}
 
-			.section_title {
-				color: #c03e62;
-				text-shadow: 1px 1px 1px #000000;
-			}
+		.section_title {
+			color: #c03e62;
+			text-shadow: 1px 1px 1px #000000;
+		}
 
-			.note {
-				bottom: 20px;
-				width: 95%;
-				padding-left: 50px;
-				padding-top: 1000px;
-			}
-		</style>
+		.note {
+			bottom: 20px;
+			width: 95%;
+			padding-left: 50px;
+			padding-top: 1000px;
+		}
+	</style>
 
-		<script>
-			$(document).ready(function() {
-				$('#tab1').click(function() {
-					$('#RealEstateTab').css({
-						'display' : 'block'
-					});
-					$('#tabs').css({
-						'display' : 'none'
-					});
+	<script>
+		$(document).ready(function() {
+			$('#tab1').click(function() {
+				$('#RealEstateTab').css({
+					'display' : 'block'
 				});
-				$('#tab2').click(function() {
-					$('#RealEstateTab').css({
-						'display' : 'none'
-					});
-					$('#tabs').css({
-						'display' : 'block'
-					});
+				$('#tabs').css({
+					'display' : 'none'
 				});
 			});
-		</script>
+			$('#tab2').click(function() {
+				$('#RealEstateTab').css({
+					'display' : 'none'
+				});
+				$('#tabs').css({
+					'display' : 'block'
+				});
+			});
+		});
+	</script>
 
-	
-
-		<script>
-			function showtemplate(temp) {
-				$.ajax({
-					type : "POST",
-					url : 'showLocations.php',
-					data : {
-						'section' : temp
-					},
-					success : function(data) {
-						$("#somewhere").html(data);
+	<script>
+		$(function() {
+			$('#gi').hide();
+			$('#katoikia').hide();
+			$('#epxwros').hide();
+			$('#ksenodoxeio').hide();
+			$('#category').change(function() {
+				if ($('#category').val() == 'Γη') {
+					$('#gi').show();
+					$('#katoikia').hide();
+					$('#epxwros').hide();
+					$('#ksenodoxeio').hide();
+					$('#nothing').hide();
+					$('#gi').children(":input").prop("disabled", false);
+						// enable
+						$('#katoikia').children(":input").prop("disabled", true);
+						// disable
+						$('#epxwros').children(":input").prop("disabled", true);
+						// disable
+						$('#ksenodoxeio').children(":input").prop("disabled", true);
+						// disable
+					} else if ($('#category').val() == 'Κατοικία') {
+						$('#gi').hide();
+						$('#katoikia').show();
+						$('#epxwros').hide();
+						$('#ksenodoxeio').show();
+						$('#nothing').hide();
+						$('#gi').children(":input").prop("disabled", true);
+						// disable
+						$('#katoikia').children(":input").prop("disabled", false);
+						// enable
+						$('#epxwros').children(":input").prop("disabled", true);
+						// disable
+						$('#ksenodoxeio').children(":input").prop("disabled", false);
+						// enable
+					} else if ($('#category').val() == 'Επαγγελματικός χώρος') {
+						$('#gi').hide();
+						$('#katoikia').hide();
+						$('#epxwros').show();
+						$('#ksenodoxeio').show();
+						$('#nothing').hide();
+						$('#gi').children(":input").prop("disabled", true);
+						// disable
+						$('#katoikia').children(":input").prop("disabled", true);
+						// disable
+						$('#epxwros').children(":input").prop("disabled", false);
+						// enable
+						$('#ksenodoxeio').children(":input").prop("disabled", false);
+						// enable
+					} else if ($('#category').val() == 'Ξενοδοχείο') {
+						$('#gi').hide();
+						$('#katoikia').hide();
+						$('#epxwros').hide();
+						$('#ksenodoxeio').show();
+						$('#nothing').hide();
+						$('#gi').children(":input").prop("disabled", true);
+						// disable
+						$('#katoikia').children(":input").prop("disabled", true);
+						// disable
+						$('#epxwros').children(":input").prop("disabled", true);
+						// disable
+						$('#ksenodoxeio').children(":input").prop("disabled", false);
+						// enable
+					} else {
+						$('#gi').hide();
+						$('#katoikia').hide();
+						$('#epxwros').hide();
+						$('#ksenodoxeio').hide();
+						$('#nothing').show();
 					}
 				});
-			}
-		</script>
+		});
 
-	</head>
+	</script>
 
-	<body>
+	<script>
+		function showtemplate(temp) {
+			$.ajax({
+				type : "POST",
+				url : 'showLocations2.php',
+				data : {
+					'section' : temp
+				},
+				success : function(data) {
+					$("#somewhere").html(data);
+				}
+			});
+		}
+	</script>
 
-		<div id="wrapper">
+	<script>
+		function submitForms() {
+			document.forms["form1"].submit();
+			document.forms["form2"].submit();
 
-			<!-- Navigation -->
-			<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">Προσθήκη νέου ακινήτου</a>
-				</div>
-				<!-- Top Menu Items -->
-				<ul class="nav navbar-right top-nav">
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['id']; ?>
+		});
+	</script>
+
+</head>
+
+<body>
+
+	<div id="wrapper">
+
+		<!-- Navigation -->
+		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Προσθήκη νέου ακινήτου</a>
+			</div>
+			<!-- Top Menu Items -->
+			<ul class="nav navbar-right top-nav">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['id']; ?>
 						<b class="caret"></b></a>
 						<ul class="dropdown-menu">
 							<li>
@@ -275,7 +362,7 @@
 
 						</li>
 						<li>
-							<a href="calendar_broker.php"><i class="fa fa-fw fa-calendar"></i>Ημερολόγιο</a>
+							<a target="_blank" href="http://accounts.google.com/AccountChooser?continue=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Frender%3Fpli%3D1&hl=en&service=cl&scc=1"><i class="fa fa-fw fa-calendar"></i>Ημερολόγιο</a>
 						</li>
 
 					</ul>
@@ -299,7 +386,7 @@
 						<form role="form" id="form1" action="addProperty.php" method="post">
 							<div class="form-group">
 								<h3 class="section_title">Κατηγορία *</h3>
-								<select name="category" class="form-control" required id="category" >
+								<select name="category" class="form-control" id="category" >
 									<option value="nothing">--</option>
 									<option value="Γη">Γη</option>
 									<option  value="Κατοικία">Κατοικία</option>
@@ -449,7 +536,23 @@
 											<br>
 											<label>Ανέβασε φωτογραφίες</label>
 											<input type="file" name="photo">
-
+											<div>
+												<br><label>Εμφάνιση</label><br>
+												<?php $sql="SELECT * FROM REAppearanceChoices";
+												$result = mysqli_query($conn, $sql);
+												$i=0;
+												while($row=mysqli_fetch_assoc($result)) {
+													if ($i<5) {
+														echo "<div class='col-md-6 checkbox'>";
+														if ($i==1)
+															echo "<br>";
+														echo"<input name='REA".$row['AppearanceNo']."' type='checkbox' value='".$row['AppearanceNo']."'> ".$row['Description']."</div>";
+													}
+													else
+														echo "<div class='col-md-6 checkbox'><input name='REA".$row['AppearanceNo']."' type='checkbox' value='".$row['AppearanceNo']."'> ".$row['Description']."</div>";
+													$i++;
+												} ?>
+											</div>
 										</div><!--end of 1i stili-->
 
 										<div class="col-md-6">
@@ -499,7 +602,7 @@
 											<div class="col-md-6">
 												<label>Αριθμός τεμαχίου:</label>
 												<span runat="server" ID="required" style="color:Red;" visible="false"> *</span>
-												<input type="text" class="form-control" required name="piece_no">
+												<input type="text" required class="form-control" name="piece_no">
 												<br>
 											</div>
 											<div class="col-md-6">
@@ -549,8 +652,24 @@
 												<option value="0">No</option>
 												<br>
 											</select>
+											<div>
+											<br><label>Θέα</label><br>
+											<?php $sql="SELECT * FROM REViewChoices";
+											$result = mysqli_query($conn, $sql);
+											$i=0;
+											while($row=mysqli_fetch_assoc($result)) {
+												if ($i<3) {
+													echo "<div class='col-md-6 checkbox'>";
+													if ($i==1)
+														echo "<br>";
+													echo"<input name='REV".$row['ViewNo']."' type='checkbox' value='".$row['ViewNo']."'> ".$row['Description']."</div>";
+												}
+												else
+													echo "<div class='col-md-6 checkbox'><input name='REV".$row['ViewNo']."' type='checkbox' value='".$row['ViewNo']."'> ".$row['Description']."</div>";
+												$i++;
+											} ?>
 										</div>
-
+										</div>
 										<br>
 										<br>
 										<br>
@@ -562,22 +681,22 @@
 											</button>
 										</div>
 									</div>
-						</form>
-						<!-- End of first "tab" -->
+								</form>
+								<!-- End of first "tab" -->
+
+							</div>
+							<!-- /.main wrap -->
+						</div>
+						<!-- /. End of section : TABS (content) -->
 
 					</div>
-					<!-- /.main wrap -->
+					<!-- /.row -->
+
 				</div>
-				<!-- /. End of section : TABS (content) -->
+				<!-- /.container-fluid -->
 
 			</div>
-			<!-- /.row -->
-
-		</div>
-		<!-- /.container-fluid -->
-
-		</div>
-		<!-- /#page-wrapper -->
+			<!-- /#page-wrapper -->
 
 		</div>
 		<!-- /#wrapper -->
@@ -590,4 +709,4 @@
 
 	</body>
 
-</html>
+	</html>
